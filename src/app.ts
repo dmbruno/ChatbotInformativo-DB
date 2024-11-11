56a3f41cab0c1322b7f61034b38ab726873e3072
Diego Bruno <dmbruno61@gmail.com>
2024-11-11 14:25:20 -0300

    configurando dirname

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import { MemoryDB as Database } from '@builderbot/bot';
 import 'dotenv/config';
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const PORT = process.env.PORT;
